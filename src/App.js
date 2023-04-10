242ab2509b577b7b4d633dd88009feae3d912cbb
Evelinanazarenko <evelinzrnk@icloud.com>
2023-04-10 13:37:41 +0200

    added form and css properties

--- src/App.js
+++ src/App.js
@@ -4,25 +4,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <div className="container mt-5 border rounded w-50">
-        <h1>Weather in Sydney</h1>
+      <div className="container mt-5 border rounded pb-4">
+        <form>
+          <div className="row mt-3 mb-3">
+            <div className="col-9">
+              <input type="search" placeholder="Type the city.." className="form-control" />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary w-100" />
+            </div>
+
+          </div>
+        </form>
+        <div className="discription-date">
+          <h1>Sydney</h1>
+          <ul>
+            <li>Monday</li>
+            <li>Partly cloudy</li>
+          </ul>
+        </div>
         <div className="row">
-          <div className="col-6 d-flex">
+          <div className="col-6 d-flex align-items-center first-column-descriprion">
             <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather discription" />
             <span>19</span>
             <div>
               <a href="/">℃</a>|<a href="/">℉</a>
             </div>
+          </div>
+          <div className="col-6 second-column-descriprion">
             <ul>
               <li>Precipitation:</li>
               <li>Humidity:</li>
               <li>Wind:</li>
             </ul>
-          </div>
-          <div className="col-6">
-            <p>Weather</p>
-            <span className="d-block">Monday</span>
-            <span className="d-block">Partly cloudy</span>
           </div>
         </div>
       </div>
